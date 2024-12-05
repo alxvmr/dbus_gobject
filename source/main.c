a2574377243e68e580773908d8489ca516808a5d
Maria Alexeeva <alxvmr@altlinux.org>
2024-12-05 14:44:09 +0400

    add on_bus_acquired

--- source/main.c
+++ source/main.c
@@ -1,6 +1,5 @@
 #include "../include/alxvmrexample.h"
 
-static GMainLoop *loop = NULL;
 #define SERVICE_PATH "/org/alxvmr/example"
 #define SERVICE_NAME "org.alxvmr.example"
 
@@ -52,6 +51,32 @@ static const GDBusInterfaceVTable interface_vtable =
     .set_property = NULL
 };
 
+static void on_bus_acquired (GDBusConnection *bus, 
+                             const gchar *name,
+                             gpointer user_data)
+{
+    GError *error = NULL;
+    guint registration_id;
+    AlxvmrExample *self = (AlxvmrExample *) user_data;
+    g_assert(self != NULL);
+
+    registration_id = g_dbus_connection_register_object(bus,
+                                                        SERVICE_PATH,
+                                                        alxvmr_example_get_introspection_data(self)->interfaces[0],
+                                                        &interface_vtable,
+                                                        g_object_ref(self),
+                                                        g_object_unref,
+                                                        error);
+
+    if (error) {
+        g_critical("Failed to register object: %s", error->message);
+        g_error_free(error);
+        return;
+    }
+
+    g_assert(registration_id > 0);
+}
+
 
 int main (int argc, char *argv[])
 {
@@ -59,35 +84,26 @@ int main (int argc, char *argv[])
     AlxvmrExample *obj = alxvmr_example_new();
     GError *error = NULL;
     guint registration_id;
-    GBusNameOwnerFlags request_name_result;
+    guint owner_id;
 
     bus = g_bus_get_sync(G_BUS_TYPE_SESSION, NULL, &error);
     if (bus == NULL) {
         g_critical("error getting session bus: %s", error->message);
     }
 
-    registration_id = g_dbus_connection_register_object(bus,
-                                                        SERVICE_PATH,
-                                                        alxvmr_example_get_introspection_data(obj)->interfaces[0],
-                                                        &interface_vtable,
-                                                        g_object_ref(obj),
-                                                        g_object_unref,
-                                                        error);
-
-    g_assert (registration_id > 0);
-
-    request_name_result = g_bus_own_name(G_BUS_TYPE_SESSION,
-                                         SERVICE_NAME,
-                                         G_BUS_NAME_OWNER_FLAGS_NONE,
-                                         NULL,
-                                         NULL,
-                                         NULL,
-                                         bus,
-                                         NULL);
-
+    owner_id = g_bus_own_name(G_BUS_TYPE_SESSION,
+                              SERVICE_NAME,
+                              G_BUS_NAME_OWNER_FLAGS_NONE,
+                              on_bus_acquired,
+                              NULL,
+                              NULL,
+                              obj,
+                              NULL);
+                              
+    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
     loop = g_main_loop_new (NULL, FALSE);
     g_main_loop_run (loop);
-    g_bus_unown_name (request_name_result);
+    g_bus_unown_name (owner_id);
     
     return 0;
 }
