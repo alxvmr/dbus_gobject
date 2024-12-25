321af5b008edfef568823feac743db879a8acc4b
Maria Alexeeva <alxvmr@altlinux.org>
2024-12-25 15:38:06 +0400

    Logic of work with json is placed in a separate file

--- source/pam_helper.c
+++ source/pam_helper.c
@@ -1,8 +1,7 @@
 #include <security/pam_appl.h>
 #include "../include/passwduser.h"
-#include "../include/translate.h"
+#include "../include/pam_helper_json.h"
 #include <stdio.h>
-#include <json-glib/json-glib.h>
 
 #define	PASSWD_SERVICE	"passwd"
 #define PAM_OLDPASS 0
@@ -95,12 +94,9 @@ non_interactive_conv (int                        num_msg,
 }
 
 int
-wrapped_pam_end (pam_handle_t *pamh, int retval, JsonObject *nested)
+wrapped_pam_end (pam_handle_t *pamh, int retval, JsonObject *object)
 {
-    json_object_set_int_member (nested, "pam_status_code", retval);
-    json_object_set_string_member (nested, "pam_status_mess_en", pam_strerror (pamh, retval));
-    json_object_set_string_member (nested, "pam_status_mess_ru", get_translate_by_pam_retval (pamh, retval));
-
+    set_member_pam (object, "pam_end", retval, pamh);
     return pam_end (pamh, retval);
 }
 
@@ -112,107 +108,34 @@ setup_pam (PasswdUser *user, JsonObject *object)
     pam_handle_t *pamh = NULL;
     struct pam_conv conv = { non_interactive_conv, user };
     int retval;
-    JsonObject *nested = NULL;
 
     retval = pam_start (PASSWD_SERVICE, user->user_name, &conv, &pamh);
-
-    nested = json_object_get_object_member (object, "pam_start");
-    json_object_set_int_member (nested, "pam_status_code", retval);
-    json_object_set_string_member (nested, "pam_status_mess_en", pam_strerror (pamh, retval));
-    json_object_set_string_member (nested, "pam_status_mess_ru", get_translate_by_pam_retval (pamh, retval));
+    set_member_pam (object, "pam_start", retval, pamh);
 
     if (retval != PAM_SUCCESS) {
-        nested = json_object_get_object_member (object, "pam_end");
-        wrapped_pam_end (pamh, retval, nested);
+        wrapped_pam_end (pamh, retval, object);
         return retval;
     }
 
     retval = pam_chauthtok (pamh, 0);
-
-    nested = json_object_get_object_member (object, "pam_chauthtok");
-    json_object_set_int_member (nested, "pam_status_code", retval);
-    json_object_set_string_member (nested, "pam_status_mess_en", pam_strerror (pamh, retval));
-    json_object_set_string_member (nested, "pam_status_mess_ru", get_translate_by_pam_retval (pamh, retval));
+    set_member_pam (object, "pam_chauthtok", retval, pamh);
 
     if (retval != PAM_SUCCESS) {
         if (CONV_ERROR != NULL) {
             json_object_set_string_member(object, "pam_conv", CONV_ERROR);
         }
-        nested = json_object_get_object_member (object, "pam_end");
-        wrapped_pam_end (pamh, retval, nested);
+        wrapped_pam_end (pamh, retval, object);
         return retval;
     }
 
-    nested = json_object_get_object_member (object, "pam_end");
-    wrapped_pam_end (pamh, PAM_SUCCESS, nested);
+    wrapped_pam_end (pamh, PAM_SUCCESS, object);
 
     if (retval != PAM_SUCCESS) {
-        wrapped_pam_end (pamh, retval, nested);
+        wrapped_pam_end (pamh, retval, object);
         return retval;
     }
 
     return PAM_SUCCESS;
-}
-
-JsonNode *
-init_json_node ()
-{
-    JsonNode *root = json_node_new (JSON_NODE_OBJECT);
-    JsonObject *object = json_object_new ();
-    GList *members = NULL;
-
-    json_object_set_member (object, "user_name", json_node_new(JSON_NODE_NULL));
-    json_object_set_member (object, "main_error", json_node_new(JSON_NODE_NULL));
-    json_object_set_member (object, "pam_start", json_node_new(JSON_NODE_NULL));
-    json_object_set_member (object, "pam_chauthtok", json_node_new(JSON_NODE_NULL));
-    json_object_set_member (object, "pam_conv", json_node_new(JSON_NODE_NULL));
-    json_object_set_member (object, "pam_end", json_node_new(JSON_NODE_NULL));
-    // TODO: добавлять пароли в json?
-
-    members = json_object_get_members (object);
-    for (GList *l = members; l != NULL; l = l->next) {
-        const gchar *key = (const gchar *) l->data;
-        if (g_strcmp0 (key, "user_name") && g_strcmp0 (key, "main_error") && g_strcmp0 (key, "pam_conv")) {
-            JsonObject *nested = json_object_new ();
-            json_object_set_member (nested, "pam_status_code", json_node_new(JSON_NODE_NULL));
-            json_object_set_member (nested, "pam_status_mess_en", json_node_new(JSON_NODE_NULL));
-            json_object_set_member (nested, "pam_status_mess_ru", json_node_new(JSON_NODE_NULL));
-
-            json_object_set_object_member (object, key, nested);
-        }
-    }
-
-    g_list_free (members);
-    json_node_init (root, JSON_NODE_OBJECT);
-    json_node_set_object (root, object);
-
-    return root;
-}
-
-gchar *
-get_string_from_json_node (JsonNode *root)
-{
-    JsonGenerator *generator = json_generator_new();
-    json_generator_set_root (generator, root);
-    gchar *json_string = json_generator_to_data (generator, NULL);
-
-    g_object_unref (generator);
-
-    return json_string;
-}
-
-void
-clear_json_object (JsonObject *object)
-{
-    GList *members = json_object_get_members (object);
-
-    for (GList *l = members; l != NULL; l = l->next) {
-        const gchar *key = (const gchar *) l->data;
-        json_object_remove_member (object, key);
-    }
-
-    g_list_free (members);
-    json_object_unref (object);
 }
 
 /*
